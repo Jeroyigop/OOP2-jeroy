facab0b86f3175519c7fe2b214a7e7ebe4df6224
Jeroyigop <jerie.corales@lorma.edu>
2026-04-01 11:26:57 +0800

    sdbgiwebiedhbvwadsfiwqbvjhi

---
+++ activity10/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        GeometricObject circle = new Circle(5);
+        GeometricObject rectangle = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+    }
+}
