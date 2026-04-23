18548ad513b30948b3ff94b40df8cbfa19ce281f
Jeroyigop <jerie.corales@lorma.edu>
2026-04-23 12:21:54 +0800

    trtytry

---
+++ oopact14/Competitor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public interface Competitor {
+    void playMatch();
+
+    default void reportStatus() {
+        System.out.println("[Status] Competitor is ready for the next round.");
+    }
+
+    static boolean isValidScore(int score) {
+        return score >= 0 && score <= 100;
+    }
+}
